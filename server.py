fbc49f7d1d8d80a9f67f7b359038691955e907ab
Mike Kane <mike.kane@students.makeschool.com>
2015-10-15 12:58:46 -0700

    adds put and delete for trips class

--- server.py
+++ server.py
@@ -10,33 +10,83 @@ mongo = MongoClient('localhost', 27017)
 app.db = mongo.develop_database
 api = Api(app)
 
-#Implement REST Resource
-class MyObject(Resource):
+
+# Implement REST Resource
+class Trip(Resource):
 
     def post(self):
-      new_myobject = request.json
-      myobject_collection = app.db.myobjects
-      result = myobject_collection.insert_one(request.json)
+        new_trip = request.json
+        trip_collection = app.db.trips
+        result = trip_collection.insert_one(new_trip)
 
-      myobject = myobject_collection.find_one({"_id": ObjectId(result.inserted_id)})
+        trip = trip_collection.find_one({"_id": ObjectId(result.inserted_id)})
 
-      return myobject
+        return trip
 
-    def get(self, myobject_id):
-      myobject_collection = app.db.myobjects
-      myobject = myobject_collection.find_one({"_id": ObjectId(myobject_id)})
+    def get(self, trip_id):
+        trip_collection = app.db.trips
+        trip = trip_collection.find_one({"_id": ObjectId(trip_id)})
 
-      if myobject is None:
-        response = jsonify(data=[])
-        response.status_code = 404
-        return response
-      else:
-        return myobject
+        if trip is None:
+            response = jsonify(data=[])  # why do I need to jsonify the data?
+            response.status_code = 404
+            return response
+        else:
+            return trip
+
+    def put(self, trip_id):
+        updated_trip = request.json
+        trip_collection = app.db.trips
+        result = trip_collection.update_one(trip_id, updated_trip)
+        if result.modified_count == 0:
+            response = jsonify(data=[])
+            response.status_code = 404
+            return response
+        else:
+            return updated_trip
+
+    def delete(self, trip_id):
+        trip_collection = app.db.trips
+        result = trip_collection.delete_one(trip_id)
+        if result.deleted_count == 0:
+            response = jsonify(data=[])
+            response.status_code = 404
+            return response
+        else:
+            response = jsonify(data=[])
+            response.status_code = 200
+            return response
+
+
+class User(Resource):
+
+    def post(self):   # check if correct
+        new_user = request.json
+        user_collection = app.db.users
+        result = user_collection.insert_one(new_user)
+
+        user = user_collection.find_one({"_id": ObjectId(result.inserted_id)})
+
+        return user
+
+    def get(self, user_id):   # check if correct
+        user_collection = app.db.users
+        user = user_collection.find_one({"_id": ObjectId(user_id)})
+
+        if user is None:
+            response = jsonify(data=[])
+            response.status_code = 404
+            return response
+        else:
+            return user
+
 
 # Add REST resource to API
-api.add_resource(MyObject, '/myobject/','/myobject/<string:myobject_id>')
+api.add_resource(Trip, '/trip/', '/trip/<string:trip_id>')
+api.add_resource(User, '/user/', '/user/<string:user_id>')
 
-# provide a custom JSON serializer for flaks_restful
+
+# provide a custom JSON serializer for flasks_restful
 @api.representation('application/json')
 def output_json(data, code, headers=None):
     resp = make_response(JSONEncoder().encode(data), code)
@@ -44,6 +94,7 @@ def output_json(data, code, headers=None):
     return resp
 
 if __name__ == '__main__':
-    # Turn this on in debug mode to get detailled information about request related exceptions: http://flask.pocoo.org/docs/0.10/config/
+    # Turn this on in debug mode to get detailled information about request
+    # related exceptions: http://flask.pocoo.org/docs/0.10/config/
     app.config['TRAP_BAD_REQUEST_ERRORS'] = True
     app.run(debug=True)
